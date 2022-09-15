93d13f026536b4f3897d15cbbea72921cf9814c1
Jason Watkins <goushoryuken@gmail.com>
2022-09-15 15:54:37 -0700

    style check

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,28 +5,36 @@ const Main = () => {
     <main>
       <section className='main-section'>
         <div className='filler'></div>
-        <VDivider />
         <div className='test-main-left'>
-          <h1>STUFF</h1>
-          <h3 className='sub-title'>with other stuff</h3>
+          <h2>STUFF</h2>
+          <h3 className=''>with other stuff</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </section>
-      <div className='test-main-right'>
-        <h1>STUFF</h1>
-        <h3 className='sub-title'>with other stuff</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div className='test-main-left'>
-        <h1>STUFF</h1>
-        <h3 className='sub-title'>with other stuff</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
-      <div className='test-main-right'>
-        <h1>STUFF</h1>
-        <h3 className='sub-title'>with other stuff</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
+      <section className='main-section'>
+        <div className='test-main-right'>
+          <h2>STUFF</h2>
+          <h3 className=''>with other stuff</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div className='filler'></div>
+      </section>
+      <section className='main-section'>
+        <div className='filler'></div>
+        <div className='test-main-left'>
+          <h2>STUFF</h2>
+          <h3 className=''>with other stuff</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+      </section>
+      <section className='main-section'>
+        <div className='test-main-right'>
+          <h2>STUFF</h2>
+          <h3 className=''>with other stuff</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div className='filler'></div>
+      </section>
 
     </main>
   )
--- src/components/VDivider.js
+++ src/components/VDivider.js
@@ -4,8 +4,8 @@ import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 const VDivider = () => {
   return (
     <div className='v-div'>
-      <div className='v-top'><FontAwesomeIcon className='does-spin' icon={faAsterisk} size='2x' /></div>
-      <div className='v-bottom'><FontAwesomeIcon className='does-spin' icon={faAsterisk} size='2x' /></div>
+      <div className='v-top'><FontAwesomeIcon className='' icon={faAsterisk} size='2x' /></div>
+      <div className='v-bottom'><FontAwesomeIcon className='' icon={faAsterisk} size='2x' /></div>
     </div>
   )
 }
