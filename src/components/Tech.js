8bc7a54f0146421f4d6896a4e3fba5a7a7ec7b2f
Jason Watkins <goushoryuken@gmail.com>
2022-09-16 09:05:40 -0700

    table style

--- src/components/Tech.js
+++ src/components/Tech.js
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGem } from "@fortawesome/free-regular-svg-icons";
 import { faJsSquare } from "@fortawesome/free-brands-svg-icons";
+import { faJava } from "@fortawesome/free-brands-svg-icons";
+import { faPython } from "@fortawesome/free-brands-svg-icons";
 
 const Tech = () => {
   return (
@@ -9,6 +11,7 @@ const Tech = () => {
         <thead>
           <tr>
             <th><h3>languages</h3></th>
+            <th></th>
             <th><h3>frameworks</h3></th>
             <th><h3>ect</h3></th>
             <th><h3>ect</h3></th>
@@ -16,19 +19,32 @@ const Tech = () => {
         </thead>
         <tbody>
           <tr>
-            <td><FontAwesomeIcon icon={faGem} /><span> ruby</span></td>
-            
+            <td><span> ruby</span></td>
+            <td><FontAwesomeIcon icon={faGem} size='2x' /></td>
+            <td>ruby on rails</td>
             <td>test</td>
             <td>test</td>
           </tr>
           <tr>
-            <td><FontAwesomeIcon icon={faJsSquare} /><span> javascript</span></td>
+            <td><span> javascript</span></td>
+            <td><FontAwesomeIcon icon={faJsSquare} size='2x' /></td>
+            <td>react</td>
+            <td>test</td>
+            <td>test</td>
           </tr>
           <tr>
-            <td>python</td>
+            <td><span> python</span></td>
+            <td><FontAwesomeIcon icon={faPython} size='2x' /></td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
           </tr>
           <tr>
-            <td>java</td>
+            <td><span> java</span></td>
+            <td><FontAwesomeIcon icon={faJava} size='2x' /></td>
+            <td>test</td>
+            <td>test</td>
+            <td>test</td>
           </tr>
         </tbody>
       </table>
